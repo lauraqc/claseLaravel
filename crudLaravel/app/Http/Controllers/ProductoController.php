821e4b024e611c5a9d8bd2e006278ef13fd9644f
lauraqc <lquinterocu@unal.edu.co>
2022-10-25 11:46:25 -0500

    El controlador del producto donde se manejaran todo lo relacionado con este

---
+++ crudLaravel/app/Http/Controllers/ProductoController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Producto;
+
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productos = Producto::all();
+        return view('gestionProductos')->with('productos', $productos);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('gestionProductos');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $nombre = $request->input('nombre');
+        $precio = $request->input('precio');
+        $descripcion = $request->input('descripcion');
+
+        $producto = new Producto();
+        $producto->nombre = $nombre;
+        $producto->precio = $precio;
+        $producto->descripcion = $descripcion;
+        $producto->save();
+        return redirect()->route('producto.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $producto = Producto::find($id);
+        $producto->delete();
+        return redirect()->route('producto.index');
+    }
+}
